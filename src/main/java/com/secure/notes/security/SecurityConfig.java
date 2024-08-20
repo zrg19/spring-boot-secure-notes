8f76c49e23a1f23e143d05756b4c6513195f8a7d
Zeeshan Rasheed <zeeshan.rasheed.goraya@gmail.com>
2024-08-20 09:29:48 +0500

    Add Security Config and apply different scenarios

---
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.secure.notes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+        });
+//        http.formLogin(Customizer.withDefaults());
+        http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+}
