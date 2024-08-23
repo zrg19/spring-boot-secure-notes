6af3585f9777a8817faf98879176fef7606a873b
Zeeshan Rasheed <zeeshan.rasheed.goraya@gmail.com>
2024-08-23 08:36:28 +0500

    Optimize SecurityConfig Open logging

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,13 +18,14 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
             ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests
-                    .requestMatchers("/contact").permitAll()
-                    .requestMatchers("/public/**").permitAll()
-                    .requestMatchers("/admin/**").denyAll()
+//                    .requestMatchers("/contact").permitAll()
+//                    .requestMatchers("/public/**").permitAll()
+//                    .requestMatchers("/admin/**").denyAll()
                     .anyRequest()).authenticated();
         });
+        http.csrf(AbstractHttpConfigurer::disable);
 //        http.formLogin(Customizer.withDefaults());
-        http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
     }
